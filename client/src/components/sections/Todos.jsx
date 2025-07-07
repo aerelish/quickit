3cdbf40708e7fc85a740772c5fd25ac2f5510420
aerelish <elisharagas@gmail.com>
2025-07-07 23:40:57 +0800

    refactor: moving some of the function in Todo.jsx specific to item to TodoItem.jsx

--- client/src/components/sections/Todos.jsx
+++ client/src/components/sections/Todos.jsx
@@ -1,34 +1,19 @@
-// packages
 import { useState, useEffect } from 'react';
-
-// components
+import { useAuthContext } from '../../context/AuthContext';
+import { useScreen } from '../../hooks/useScreen';
+import { validateToken } from '../../services/authServices';
+import { getTodos, addTodo, updateTodoPriority } from '../../services/todoServices';
 import TodoItem from '../todo/TodoItem';
 import TodoForm from '../todo/TodoForm';
 
-// services
-import { validateToken } from '../../services/authServices';
-import { getTodos, addTodo, updateTodo, updateTodoPriority, deleteTodo} from '../../services/todoServices';
-
-// custom hooks
-import { useScreen } from '../../hooks/useScreen';
-
-// context
-import { useAuthContext } from '../../context/AuthContext';
-
 function Todos() {
   
-  // custom context
-  const { isTokenValid, setIsTokenValid } = useAuthContext();
-
-  // initialize hooks
   const [todos, setTodos] = useState([])
   const [newTodo, setNewTodo] = useState('');
-  const [updatedTodoTitle, setUpdatedTodoTitle] = useState('');
   const [editing, setEditing] = useState(0);
 
-  // custom hooks
+  const { isTokenValid, setIsTokenValid } = useAuthContext();
   const { isMobile } = useScreen();
-
 
   const loadTodos = async () => {
     if (!isTokenValid) {
@@ -50,88 +35,36 @@ function Todos() {
       console.error(response.message);
     };
   };
-
-  // TODO - try to move this to TodoItem instead
-
-  const updateTodoItem = async (event) => {
-    event.preventDefault();
-    const data = { title: updatedTodoTitle }
-    const response = await updateTodo(editing, data);
-    if (response.success) {
-      setEditing(null);
-      setTodos(prev =>
-        prev.map(todo =>
-          todo.id === response.data.id ? { ...todo, title: response.data.title } : todo
-        )
-      );
-    } else {
-      console.error(response.message);
-    };
+  
+  const onDeleteSuccess = (deletedId) => {
+    setTodos(prev => prev.filter(todo => todo.id !== deletedId));
   };
 
-  // TODO - try to move this to TodoItem instead
-
-  const deleteTodoItem = async (id) => {
-    const response = await deleteTodo(id);
-    if (response.success) {
-      setTodos(todos.filter(todo => todo.id !== id))
-    } else {
-      console.error(response.message);
-    };
-  };  
-
-  // TODO - try to move this to TodoItem instead
-
-  const moveUp = async (index) => {
-    if (index === 0) return;
+  const moveItem = async (sourceIndex, targetIndex) => {
     // copy of todos
     const newTodos = [...todos];
-    
-    const source = newTodos[index].id;
-    const target = newTodos[index - 1].id;
-
+    // getting ids
+    const source = newTodos[sourceIndex].id;
+    const target = newTodos[targetIndex].id;
     // source = todo that you want to increase/decrease in priority
     // target = todo that you are targeting, e.g. the one above or below
     const response = await updateTodoPriority(target, source);
     if (response.success) {
-      // swap indexes for index and index-1
-      // note: index start with 0, so moving up means -1 index
-      [newTodos[index], newTodos[index - 1]] = [newTodos[index - 1], newTodos[index]];    
-      setTodos(newTodos);
-    } else {
-      console.error(response.message);
-    }
-  };
-
-  // TODO - try to move this to TodoItem instead
-
-  const moveDown = async (index) => {
-    if (index === todos.length - 1) return;
-    // copy of todos
-    const newTodos = [...todos];
-
-    const source = newTodos[index].id;
-    const target = newTodos[index + 1].id;
-
-    // source = todo that you want to increase/decrease in priority
-    // target = todo that you are targeting, e.g. the one above or below
-    const response = await updateTodoPriority(source, target);
-    if (response.success) {
-      // swap indexes for index and index-1
-      // note: index start with 0, so moving up means -1 index
-      [newTodos[index], newTodos[index + 1]] = [newTodos[index + 1], newTodos[index]];    
+      [newTodos[sourceIndex], newTodos[targetIndex]] = [newTodos[targetIndex], newTodos[sourceIndex]];    
       setTodos(newTodos);
     } else {
       console.error(response.message);
     };
   };
 
-  // TODO - try to move this to TodoItem instead
+  const moveUp = async (index) => {
+    if (index === 0) return;
+    moveItem(index, index - 1);
+  };
 
-  // for indicating that user is editing a todo item
-  const editTodoItem = (id, title) => { 
-    setEditing(id);
-    setUpdatedTodoTitle(title);
+  const moveDown = async (index) => {
+    if (index === todos.length - 1) return;
+    moveItem(index, index + 1);
   };
 
   useEffect(() => { loadTodos() }, []);
@@ -148,26 +81,14 @@ function Todos() {
       />
       <div className='w-full'>
         {todos.slice(0, isMobile ? 3 : todos.length).map((todo, index) => (
-          todo.id === editing ? (
-            <TodoForm
-              key={todo.id}
-              onSubmit={updateTodoItem}
-              placeholder='have something todo?'
-              value={updatedTodoTitle}
-              onChange={(event) => setUpdatedTodoTitle(event.target.value)}
-              icon={'faFloppyDisk'}
-            />
-          ) : (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              index={index}
-              moveUp={moveUp}
-              moveDown={moveDown}
-              editTodoItem={editTodoItem}
-              deleteTodoItem={deleteTodoItem}
-            />
-          )
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            index={index}
+            moveUp={moveUp}
+            moveDown={moveDown}
+            onDeleteSuccess={onDeleteSuccess}
+          />
         ))}
         { isMobile && todos.length > 3 && (
           <p className='text-zinc-400 cursor-pointer'>view more...</p>
--- client/src/components/todo/TodoItem.jsx
+++ client/src/components/todo/TodoItem.jsx
@@ -1,13 +1,9 @@
-// packages and stuff
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 import { faArrowUp, faArrowDown, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
-
-// services
-import { updateTodo } from '../../services/todoServices';
-
-// components
+import { updateTodo, deleteTodo } from '../../services/todoServices';
+import TodoForm from './TodoForm';
 import ButtonIcon from '../ButtonIcon'
 
 function TodoItem({
@@ -15,13 +11,35 @@ function TodoItem({
   index, 
   moveUp, 
   moveDown, 
-  editTodoItem, 
-  deleteTodoItem
+  onDeleteSuccess,
 }){
 
   const [todoItem, setTodoItem] = useState(todo);
+  const [todoInput, setTodoInput] = useState('');
   const [isCompleted, setIsCompleted] = useState(todo.completed);
+  const [editing, setEditing] = useState(null);
 
+  const updateTodoItem = async (event) => {
+    event.preventDefault();
+    const data = { title: todoInput }
+    const response = await updateTodo(editing, data);
+    if (response.success) {
+      setTodoItem({ ...todoItem, title: response.data.title })
+      setEditing(null);
+    } else {
+      console.error(response.message);
+    };
+  };
+
+  const deleteTodoItem = async (id) => {
+    const response = await deleteTodo(id);
+    if (response.success) {
+      onDeleteSuccess(todoItem.id)
+    } else {
+      console.error(response.message);
+    };
+  };  
+  
   const setTodoCompleted = async (id, isCompleted) => {
     const data = { completed: !isCompleted }
     const response = await updateTodo(id, data);
@@ -33,39 +51,58 @@ function TodoItem({
     };
   };
 
+  const editTodoItem = (id, title) => { 
+    setEditing(id);
+    setTodoInput(title);
+  };
+  const handleUpdateTodoChange = (event) => { setTodoInput(event.target.value) }
+
   return (
     <div className='flex justify-between items-center border-b border-zinc-700 py-1.5 my-3'>
-      <div className='pl-0.5 flex gap-1 items-center'>
-        {todoItem.completed && <FontAwesomeIcon className='text-green-400 text-base' icon={faCircleCheck} />}
-        <h3 
-          className={`cursor-pointer px-0.5 ${isCompleted && 'line-through text-zinc-500'}`} 
-          onClick={() => setTodoCompleted(todoItem.id, todoItem.completed)}
-        >
-          {todo.title} 
-        </h3>
-      </div>
-      <div className='flex gap-1.5 pr-1 items-center'>   
-        <ButtonIcon
-          icon={faArrowUp}
-          onClick={() => moveUp(index)}
-          hoverColor='[var(--white)]'
-        />       
-        <ButtonIcon
-          icon={faArrowDown}
-          onClick={() => moveDown(index)}
-          hoverColor='[var(--white)]'
-        />       
-        <ButtonIcon
-          icon={faPenToSquare}
-          onClick={() => editTodoItem(todo.id, todo.title)}
-          hoverColor='[var(--accent-color)]'
-        />       
-        <ButtonIcon
-          icon={faTrash}
-          onClick={() => deleteTodoItem(todo.id)}
-          hoverColor='red-500'
-        />       
-      </div>
+      { todoItem.id === editing ? (
+          <TodoForm
+            onSubmit={updateTodoItem}
+            placeholder='have something todo?'
+            value={todoInput}
+            onChange={(event) => handleUpdateTodoChange(event)}
+            icon={'faFloppyDisk'}
+          />
+        ) : (
+          <>
+            <div className='pl-0.5 flex gap-1 items-center'>
+              {todoItem.completed && <FontAwesomeIcon className='text-green-400 text-base' icon={faCircleCheck} />}
+              <h3 
+                className={`cursor-pointer px-0.5 ${isCompleted && 'line-through text-zinc-500'}`} 
+                onClick={() => setTodoCompleted(todoItem.id, todoItem.completed)}
+              >
+                {todoItem.title} 
+              </h3>
+            </div>
+            <div className='flex gap-1.5 pr-1 items-center'>   
+              <ButtonIcon
+                icon={faArrowUp}
+                onClick={() => moveUp(index)}
+                hoverColor='[var(--white)]'
+              />       
+              <ButtonIcon
+                icon={faArrowDown}
+                onClick={() => moveDown(index)}
+                hoverColor='[var(--white)]'
+              />       
+              <ButtonIcon
+                icon={faPenToSquare}
+                onClick={() => editTodoItem(todoItem.id, todoItem.title)}
+                hoverColor='[var(--accent-color)]'
+              />       
+              <ButtonIcon
+                icon={faTrash}
+                onClick={() => deleteTodoItem(todoItem.id)}
+                hoverColor='red-500'
+              />       
+            </div>
+          </>
+        )
+      }
     </div>
   )
 }
